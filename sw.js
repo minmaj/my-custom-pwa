92ef73255f1dc950fd3f62f8411ba9fd7a8fe843
David <engdavid13@gmail.com>
2022-02-27 09:54:35 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,3 +9,7 @@ self.addEventListener('install', function(evt) {
       ]);  
   }));  
 }); 
+
+self.addEventListener('fetch', function (event) {
+    // it can be empty if you just want to get rid of that error
+});
