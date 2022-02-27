4b6d8df76ca9125610333f83f83bec4eac31a0a8
David <engdavid13@gmail.com>
2022-02-27 09:45:51 +0100

    Create sw.js
    replace this file in root directory

---
+++ sw.js
@@ -0,0 +1,11 @@
+var cache_name = 'my-custom-pwa';  
+
+self.addEventListener('install', function(evt) {  
+  evt.waitUntil(caches.open(cache_name).then(function (cache) {  
+      cache.addAll([  
+        "index.html",
+        "scripts/main.js",
+        "sw.js",
+      ]);  
+  }));  
+}); 
