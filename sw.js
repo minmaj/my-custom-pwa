c3a36b0ee295a52841fec2015a0379429aeba3f7
Minmaj <engdavid13@gmail.com>
2022-02-28 11:40:48 +0100

    testing cache strategy

--- sw.js
+++ sw.js
@@ -6,10 +6,36 @@ self.addEventListener('install', function(evt) {
         "index.html",
         "scripts/main.js",
         "sw.js",
+        "images/rubik-128x128.png",
+        "images/rubik-256x256.png"
       ]);  
   }));  
 }); 
 
-self.addEventListener('fetch', function (event) {
-    // it can be empty if you just want to get rid of that error
+/*
+* Lien utile : https://developer.mozilla.org/fr/docs/Web/Progressive_web_apps/Offline_Service_workers#r%C3%A9pondre_aux_requ%C3%AAtes
+*/
+
+self.addEventListener('fetch', function(fetch_event) {
+  fetch_event.respondWith(response_handler);
+
+  function response_handler() {
+    caches.match(fetch_event.request).then(function(cache_response) {
+      console.log('[Service Worker] Récupération de la ressource: ' + fetch_event.request.url);
+      if(cache_response) {
+        return cache_response;
+      }
+      else {
+        let server_response = fetch(fetch_event.request).then(function(server_response) {
+          return caches.open(cache_name).then(function(cache) {
+            console.log('[Service Worker] Mise en cache de la nouvelle ressource: ' + fetch_event.request.url);
+            cache.put(e.request, server_response.clone());
+            return server_response;
+          });
+        });
+
+        return server_response;
+      }
+    });
+  }
 });
