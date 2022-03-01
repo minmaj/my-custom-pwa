fb163dfd644ff86770442fd363a6b006745cf668
Minmaj <engdavid13@gmail.com>
2022-03-01 18:59:35 +0100

    fix bugs

--- sw.js
+++ sw.js
@@ -18,10 +18,8 @@ self.addEventListener('install', function(evt) {
 self.addEventListener('fetch', function(fetch_event) {
   // console.log(fetch_event);
 
-  fetch_event.respondWith(response_handler);
-
-  function response_handler() {
-    return caches.match(fetch_event.request).then(function(cache_response) {
+  fetch_event.respondWith(
+    caches.match(fetch_event.request).then(function(cache_response) {
       console.log('[Service Worker] Récupération de la ressource: ' + fetch_event.request.url);
       if(cache_response) {
         return cache_response;
@@ -35,7 +33,7 @@ self.addEventListener('fetch', function(fetch_event) {
           });
         });
       }
-    });
-  }
+    })
+  );
   
 });
